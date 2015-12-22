fec18ac292572c917cce032c4ef614c8b9f38ad6
bgn9000 <pbourgeon@gmail.com>
2015-12-22 14:41:54 +0800

    Added main.cpp to do unit test on FeedHandler code (separate lib and bin)

---
+++ src/bin/main.cpp
@@ -0,0 +1,21 @@
+#include "FeedHandler.h"
+
+int main(int argc, char **argv)
+{
+    FeedHandler feed;
+    
+    std::string line;
+    const std::string filename(argv[1]);
+    std::ifstream infile(filename.c_str(), std::ios::in);
+    
+    int counter = 0;
+    while (std::getline(infile, line)) 
+    {
+        if (feed.processMessage(line) && ++counter % 10 == 0) 
+        {
+            feed.printCurrentOrderBook(std::cerr);
+        }
+    }
+    feed.printCurrentOrderBook(std::cout);
+    return 0;
+}
