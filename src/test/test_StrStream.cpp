eda7e0db011310c94afa2a44414c9ac67230650b
bgn9000 <pbourgeon@gmail.com>
2015-12-30 16:09:03 +0800

    Added tests on StrStream strings append (> 512 characters) : slower than std::string append

--- src/test/test_StrStream.cpp
+++ src/test/test_StrStream.cpp
@@ -47,7 +47,45 @@ int main()
     
     time_span1 = 0ULL, time_span2 = 0ULL;
     nbTests = 0U;
-    rc::check("Append an unsigned int to a string (< 512 characters)", [&](std::string strOrigin, const unsigned int intToAppend) 
+    rc::check("Append a string to a string (> 512 characters)", [&]() 
+    {
+        auto size = *rc::gen::inRange<int>(0, 512);
+        std::string strOrigin = *rc::gen::container<std::string>(size, rc::gen::character<typename std::string::value_type>());
+        size = *rc::gen::inRange<int>(513, StrStream::CAPACITY_MAX-size-1);
+        auto strToAppend = *rc::gen::container<std::string>(size, rc::gen::character<typename std::string::value_type>());
+    
+        high_resolution_clock::time_point start = high_resolution_clock::now();
+        StrStream strstream;
+        strstream.append(strOrigin.c_str(), strOrigin.length());
+        strstream.append(strToAppend.c_str(), strToAppend.length());
+        high_resolution_clock::time_point end = high_resolution_clock::now();
+        time_span1 += duration_cast<nanoseconds>(end - start).count();
+        
+        start = high_resolution_clock::now();
+        std::string str(strOrigin);
+        str += strToAppend;
+        end = high_resolution_clock::now();
+        time_span2 += duration_cast<nanoseconds>(end - start).count();
+        
+        RC_LOG() /*std::cout*/
+            << "Append a string to a string (> 512 characters) strOrigin length [" << strOrigin.length() 
+            << "] strToAppend length [" << strToAppend.length() << "] strstream length [" << strstream.length()
+            << "] string length [" << str.length() << "]" << std::endl;
+        
+        ++nbTests;
+        
+        RC_ASSERT(strstream.c_str() == str);
+    });
+    if (nbTests)
+    {
+        std::cout << "Append a string to a string (> 512 characters) perfs  [" << time_span1/nbTests << "] std::string [" 
+            << time_span2/nbTests << "] (in ns)" << std::endl;
+    }
+    
+    
+    time_span1 = 0ULL, time_span2 = 0ULL;
+    nbTests = 0U;
+    rc::check("Append an unsigned int to a string (< 512 characters)", [&](std::string strOrigin, const unsigned int intToAppend)
     {
         auto len = strOrigin.length();
         while (len > FiniteStr<>::capacity() - 64)
