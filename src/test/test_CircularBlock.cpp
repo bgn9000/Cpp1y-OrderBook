ebf40ffee40dae8b62eafa574d7265d1fc7cd9cb
bgn9000 <pbourgeon@gmail.com>
2016-02-03 00:16:32 +0800

    Use shared_future to sync dual threads tests

--- src/test/test_CircularBlock.cpp
+++ src/test/test_CircularBlock.cpp
@@ -3,6 +3,7 @@
 #include "CircularBlock.h"
 
 #include <thread>
+#include <future>
 #include <cassert>
 
 #include <chrono>
@@ -80,13 +81,18 @@ int main()
     nbTests = 0U;
     rc::check("Dual threads fill and empty", [&]() 
     {
-        CircularBlock<int, 2> waitThread;
+        std::promise<void> thr1_ready;
+        std::promise<void> thr2_ready;
+        std::promise<void> ready;
+        std::shared_future<void> start(ready.get_future());
+        
         rcCircularBlock<int> block;
         const auto nb = *rc::gen::inRange<size_t>(10, block.capacity()-1);
         
         auto threaded_emptyBlock = [&]() 
         {
-            waitThread.fill(0); // tell thread ready
+            thr1_ready.set_value();
+            start.wait();
             auto j = 0UL;
             high_resolution_clock::time_point start = high_resolution_clock::now();
             for (auto i = 1UL; i <= nb; ++i)
@@ -96,19 +102,28 @@ int main()
             high_resolution_clock::time_point end = high_resolution_clock::now();
             time_span2 += (duration_cast<nanoseconds>(end - start).count()) / nb;
         };
-        std::thread thr(threaded_emptyBlock);
+        std::thread thr1(threaded_emptyBlock);
 
-        waitThread.empty(); // wait thread
-        high_resolution_clock::time_point start = high_resolution_clock::now();
-        for (auto i = 1UL; i <= nb; ++i)
+        auto threaded_fillBlock = [&]() 
         {
-            block.fill(int(i));
-        }
-        high_resolution_clock::time_point end = high_resolution_clock::now();
-        time_span1 += (duration_cast<nanoseconds>(end - start).count()) / nb;
+            thr2_ready.set_value();
+            start.wait();
+            high_resolution_clock::time_point start = high_resolution_clock::now();
+            for (auto i = 1UL; i <= nb; ++i)
+            {
+                block.fill(int(i));
+            }
+            high_resolution_clock::time_point end = high_resolution_clock::now();
+            time_span1 += (duration_cast<nanoseconds>(end - start).count()) / nb;
+        };
+        std::thread thr2(threaded_fillBlock);
         
 //        block.dontSpin();
-        thr.join();
+        thr1_ready.get_future().wait();
+        thr2_ready.get_future().wait();
+        ready.set_value();
+        thr1.join();
+        thr2.join();
         RC_ASSERT(0UL == block.getSize());
         
         ++nbTests;
@@ -123,15 +138,20 @@ int main()
     nbTests = 0U;
     rc::check("Dual threads fill and empty with aligned to cache lines (false sharing)", [&]() 
     {
-        CircularBlock<int, 2> waitThread;
+        std::promise<void> thr1_ready;
+        std::promise<void> thr2_ready;
+        std::promise<void> ready;
+        std::shared_future<void> start(ready.get_future());
+        
         typedef struct { char pad1[cacheLinesSze]; int data_ = 0; char pad2[cacheLinesSze]; } Data;
         rcCircularBlock<Data> block;
         const auto nb = *rc::gen::inRange<size_t>(10, block.capacity()-1);
         
         auto threaded_emptyBlock = [&]() 
         {
+            thr1_ready.set_value();
+            start.wait();
             auto j = Data();
-            waitThread.fill(0); // tell thread ready
             high_resolution_clock::time_point start = high_resolution_clock::now();
             for (auto i = 1UL; i <= nb; ++i)
             {
@@ -140,20 +160,29 @@ int main()
             high_resolution_clock::time_point end = high_resolution_clock::now();
             time_span2 += (duration_cast<nanoseconds>(end - start).count()) / nb;
         };
-        std::thread thr(threaded_emptyBlock);
-
-        waitThread.empty(); // wait thread
-        high_resolution_clock::time_point start = high_resolution_clock::now();
-        for (auto i = 1UL; i <= nb; ++i)
+        std::thread thr1(threaded_emptyBlock);
+        
+        auto threaded_fillBlock = [&]() 
         {
-            Data d; d.data_ = i;
-            block.fill(std::move(d));
-        }
-        high_resolution_clock::time_point end = high_resolution_clock::now();
-        time_span1 += (duration_cast<nanoseconds>(end - start).count()) / nb;
+            thr2_ready.set_value();
+            start.wait();
+            high_resolution_clock::time_point start = high_resolution_clock::now();
+            for (auto i = 1UL; i <= nb; ++i)
+            {
+                Data d; d.data_ = i;
+                block.fill(std::move(d));
+            }
+            high_resolution_clock::time_point end = high_resolution_clock::now();
+            time_span1 += (duration_cast<nanoseconds>(end - start).count()) / nb;
+        };
+        std::thread thr2(threaded_fillBlock);
         
 //        block.dontSpin();
-        thr.join();
+        thr1_ready.get_future().wait();
+        thr2_ready.get_future().wait();
+        ready.set_value();
+        thr1.join();
+        thr2.join();
         RC_ASSERT(0UL == block.getSize());
         
         ++nbTests;
