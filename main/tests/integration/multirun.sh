d32a00e4d9a6ebb000401c47c08020d0c44f6267
olibre <olibre@Lmap.org>
2016-02-06 00:59:52 +0100

    Reset path of executables FeedHandler.out (TODO)

--- main/tests/integration/multirun.sh
+++ main/tests/integration/multirun.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash -e
+# -e => Exit as soon as an error occurs
 
 counter=1000
 step=$(( counter / 10 ))
@@ -8,11 +9,11 @@ perfmmap=0
 perfReadSB=0
 perfGetLine=0
 while [ $counter -gt 0 ]; do
-    perfnew=$(( $perfnew + $(echo "Over new:" `../src/bin/FeedHandler.out ../tests/test1.txt 2> /dev/null | grep Over` | cut -d " " -f8) ));
-    perf1=$(( $perf1 + $(echo "Over 1.0:" `../src/FeedHandler_1.0.out ../tests/test1.txt 2> /dev/null | grep Over` | cut -d " " -f8) ));
-    perfmmap=$(( $perfmmap + $(echo "Over mmap:" `../src/FeedHandler_mmap.out ../tests/test1.txt 2> /dev/null | grep Over` | cut -d " " -f9) ));
-#    perfReadSB=$(( $perfReadSB + $(echo "Over readSB:" `../src/FeedHandler_readSB.out ../tests/test1.txt 2> /dev/null | grep Over` | cut -d " " -f8) ));
-#    perfGetLine=$(( $perfGetLine + $(echo "Over getline:" `../src/FeedHandler_getline.out ../tests/test1.txt 2> /dev/null | grep Over` | cut -d " " -f8) ));
+    perfnew=$((     $perfnew     + $(echo "Over new:"     $(bin/FeedHandler.out            ../tests/test1.txt 2> /dev/null | grep Over) | cut -d " " -f8) ));
+    perf1=$((       $perf1       + $(echo "Over 1.0:"     $(bin/FeedHandler_1.0.out        ../tests/test1.txt 2> /dev/null | grep Over) | cut -d " " -f8) ));
+    perfmmap=$((    $perfmmap    + $(echo "Over mmap:"    $(bin/FeedHandler_mmap.out       ../tests/test1.txt 2> /dev/null | grep Over) | cut -d " " -f9) ));
+#   perfReadSB=$((  $perfReadSB  + $(echo "Over readSB:"  $(../src/FeedHandler_readSB.out  ../tests/test1.txt 2> /dev/null | grep Over) | cut -d " " -f8) ));
+#   perfGetLine=$(( $perfGetLine + $(echo "Over getline:" $(../src/FeedHandler_getline.out ../tests/test1.txt 2> /dev/null | grep Over) | cut -d " " -f8) ));
     counter=$(( $counter - 1 ));
     if [[ $(( counter % $step )) == 0 ]]; then
         echo $perfnew $perf1 $perfmmap
